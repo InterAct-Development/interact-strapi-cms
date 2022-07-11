b583aac5e53331ba77fafc51a2bdf8b5ecff5b50
Ross MacDonald <42966821+RossmacD@users.noreply.github.com>
2022-07-11 20:18:44 +0100

    Added Localisation
    Co-authored-by: Shane Kelly <shane.kelly2408@gmail.com>

---
+++ src/api/action-translation/controllers/action-translation.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  action-translation controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::action-translation.action-translation');
---
+++ src/api/action-translation/routes/action-translation.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * action-translation router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::action-translation.action-translation');
